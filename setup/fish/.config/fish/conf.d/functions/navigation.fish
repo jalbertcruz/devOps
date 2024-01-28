cebcb3b502d4080c8ce67e123e00ed0ed3347c19
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2024-01-28 23:18:24 +0100

    Update

--- setup/fish/.config/fish/conf.d/functions/navigation.fish
+++ setup/fish/.config/fish/conf.d/functions/navigation.fish
@@ -17,13 +17,15 @@ function rgf
   | fzf --delimiter : --preview 'bat --color=always {1} --line-range {4}:+{5} --highlight-line {2} --wrap=character --terminal-width=80' \
   | hck -Ld':' -f1,2,3 -D=":")
   if [ "$res" ]
-      rg $argv[1] > /dev/null; and command code --reuse-window --goto $res &
+      command code --reuse-window --goto $res > /dev/null &
       disown
   end
 end
 
 function rgfnv
-  rg $argv[1] > /dev/null; and rg $argv[1] --json | ripgrep_to_fzf_filter --rb 3 --lc 6 \
-  | fzf --delimiter : --preview 'bat --color=always {1} --line-range {4}:+{5} --highlight-line {2} --wrap=character --terminal-width=80' \
-  | _vim-translator | xargs nvim
+  set res (rg $argv[1] > /dev/null; and rg $argv[1] --json | ripgrep_to_fzf_filter --rb 3 --lc 6 \
+  | fzf --delimiter : --preview 'bat --color=always {1} --line-range {4}:+{5} --highlight-line {2} --wrap=character --terminal-width=80')
+  if [ "$res" ]
+      echo -n $res | _vim-translator | xargs nvim
+  end
 end
